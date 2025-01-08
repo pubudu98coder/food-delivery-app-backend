f36216ae889ca7db7bbfac008af179d3fda0c744
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2025-01-09 01:03:01 +0530

    added roleList to the user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    roleList:{type: Array, required:true},
+    roleList:{type: [Number], default:[2001]},
     refreshToken:{type:String},
     cartData: {type: Object, default: {}}
 },{minimize: false});
